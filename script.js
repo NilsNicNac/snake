979e0d78f53ab2a853a810049b68043a985fac0c
nnnac <nils@nnnac.xyz>
2024-05-30 11:37:52 +0200

    Coordinates are now handeled based on tiles not on pixels.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-playing = false;
+
+//config
+fieldWidth = 510;
+fieldHeight = 255;  //fieldHeight AND fieldWidth MUST BE MULTIPLE OF tileSize!!
+tileSize = 15;
+fps = 5;            //controls speed
+
+widthByTile = fieldWidth/tileSize;
+heightByTile = fieldHeight/tileSize;
 
 let snake = [ 
-    {x: 30, y: 0}, //head
-    {x: 15, y: 0}, //segment
-    {x: 0, y: 0} // tail
+    {x: 2, y: 0},  //head
+    {x: 1, y: 0},  //segment
+    {x: 0, y: 0}    //tail
 ];
+
+//inputQueue to allow multiple inputs before next frame has rendered. Can't be more than two elements.
 let inputQueue = [];
 
-var apple = {x: Math.floor(Math.random()*34)*15, y: Math.floor(Math.random()*17)*15};
+var apple = {
+    x: Math.floor(Math.random()*widthByTile)*tileSize, 
+    y: Math.floor(Math.random()*heightByTile)*tileSize
+};
 
 var dir = "right";
 
@@ -40,16 +53,16 @@ function move() {
     let headY = snake[0].y;
 
     if(dir === "right") {
-        headX += 15;
+        headX += 1;
     }
     if(dir === "left") {
-        headX -= 15;
+        headX -= 1;
     }
     if(dir === "up") {
-        headY -= 15;
+        headY -= 1;
     }
     if(dir === "down") {
-        headY += 15;
+        headY += 1;
     }
 
     snake.unshift({x: headX, y: headY});
@@ -63,21 +76,20 @@ function move() {
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "red";
-
-    ctx.fillRect(snake[0].x, snake[0].y, 12, 12);
+    
+    ctx.fillRect(snake[0].x * tileSize, snake[0].y * tileSize, 12, 12);
     ctx.fillStyle = "orange";
     for(i = 1; i < snake.length; i++) {
-        ctx.fillRect(snake[i].x,snake[i].y, 12, 12);
+        ctx.fillRect(snake[i].x * tileSize, snake[i].y * tileSize, 12, 12);
     }
-    console.log("x:", snake[0].x, "y:", snake[0].y)
-    spawnApple();
     
-    if (snake[0].x > 480 || snake[0].x < 0 || snake[0].y > 480 || snake[0].y < 0 ){
+    
+    if (snake[0].x >= widthByTile || snake[0].x < 0 || snake[0].y >= heightByTile || snake[0].y < 0 ){
         gameOver();
         clearInterval(intervalId);
     }
     if (snake[0].x === apple.x && snake[0].y === apple.y){
-        apple = {x: Math.floor(Math.random()*34)*15, y: Math.floor(Math.random()*17)*15};
+        apple = {x: Math.floor(Math.random()*widthByTile), y: Math.floor(Math.random()*widthByTile)};
         score += 1;
         ate = true;
         document.getElementById("score").innerHTML = "Score: " + score;
@@ -90,48 +102,45 @@ function move() {
             clearInterval(intervalId);
         }
     }
+    spawnApple();
 }
 
 function spawnApple(){
     const canvas = document.getElementById("field");
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "yellow";
-    ctx.fillRect(apple.x+2, apple.y+2, 8, 8);
+    ctx.fillRect(apple.x*tileSize + 1, apple.y*tileSize + 1, tileSize-4, tileSize-4);
 }
-
-
 
 function startGame(){
     document.getElementById("start").style.display = "none";
     document.getElementById("game-over").style.display = "none";
 
-    intervalId = setInterval(move, 200);
+    intervalId = setInterval(move, Math.round((1/fps)*1000));
 
     snake = [ 
-        {x: 60, y: 0}, //head
-        {x: 30, y: 0}, //segment
+        {x: 2, y: 0}, //head
+        {x: 1, y: 0}, //segment
         {x: 0, y: 0} // tail
     ];
     inputQueue = []
-    
-    apple = {x: Math.floor(Math.random()*34)*15, y: Math.floor(Math.random()*17)*15};
+   
+    apple = {x: Math.floor(Math.random() * widthByTile), y: Math.floor(Math.random() * heightByTile)};
     
     dir = "right";
     
     score = 0;
     ate = false;
 
-    playing = true;
 }
 
 function gameOver(){
     document.getElementById("game-over").style.display = "flex";
-    playing = false;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    field.height = 255;
-    field.width = 510;
+    field.height = fieldHeight;
+    field.width = fieldWidth;
 });
 
 document.addEventListener("keydown", (event) => {
