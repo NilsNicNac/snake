baff85cf45ef8c4749d3b7eef646bcfb85e2e921
nnnac <nils@nnnac.xyz>
2024-06-06 11:04:06 +0200

    Fixed score not resetting when restarting game

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 
 //config
-<<<<<<< HEAD
 fieldWidth = 1600;
 fieldHeight = 800;  //fieldHeight AND fieldWidth MUST BE MULTIPLE OF tileSize!!
 tileSize = 40;
-=======
-fieldWidth = 1680;
-fieldHeight = 840;  //fieldHeight AND fieldWidth MUST BE MULTIPLE OF tileSize!!
-tileSize = 60;
->>>>>>> 3a552c1886d76bd23382b54e3b2cd5b70e5954e8
 fps = 5;            //controls speed
 
 widthByTile = fieldWidth/tileSize;
@@ -34,17 +28,8 @@ var score = 0;
 var ate = false;
 
 function move() {
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     console.log(inputQueue.length)
 
->>>>>>> 3a552c1886d76bd23382b54e3b2cd5b70e5954e8
-=======
-    console.log(inputQueue.length)
-
->>>>>>> 3a552c1886d76bd23382b54e3b2cd5b70e5954e8
     badInput = false;
     if(inputQueue.length > 0){
         if(inputQueue[inputQueue.length - 1] === "right" && dir === "left" ){
@@ -107,21 +92,11 @@ function move() {
     const canvas = document.getElementById("field");
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "red";
-    
-<<<<<<< HEAD
-    ctx.fillRect(snake[0].x * tileSize, snake[0].y * tileSize, 39, 39);
-    ctx.fillStyle = "orange";
+    ctx.fillStyle = "green";
+    ctx.fillRect(snake[0].x * tileSize - 5, snake[0].y * tileSize - 5, 45, 45);
     for(i = 1; i < snake.length; i++) {
-        ctx.fillRect(snake[i].x * tileSize, snake[i].y * tileSize, 39, 39);
-=======
-    ctx.fillRect(snake[0].x * tileSize, snake[0].y * tileSize, 50, 50);
-    ctx.fillStyle = "orange";
-    for(i = 1; i < snake.length; i++) {
-        ctx.fillRect(snake[i].x * tileSize, snake[i].y * tileSize, 50, 50);
->>>>>>> 3a552c1886d76bd23382b54e3b2cd5b70e5954e8
+        ctx.fillRect(snake[i].x * tileSize, snake[i].y * tileSize, 35, 35);
     }
-    
     
     if (snake[0].x === apple.x && snake[0].y === apple.y){
         apple = {x: Math.floor(Math.random()*widthByTile), y: Math.floor(Math.random()*heightByTile)};
@@ -130,8 +105,6 @@ function move() {
         document.getElementById("score").innerHTML = "Score: " + score;
     }
 
-    //Check if head coordinates exist in snake arr
-    
     spawnApple();
 }
 
@@ -139,16 +112,14 @@ function spawnApple(){
     const canvas = document.getElementById("field");
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "yellow";
-<<<<<<< HEAD
     ctx.fillRect(apple.x*tileSize + 7, apple.y*tileSize + 7, tileSize-15, tileSize-15); 
-=======
-    ctx.fillRect(apple.x*tileSize + 10, apple.y*tileSize + 10, tileSize-30, tileSize-30); //tilesize offset needs to be 2*tilesize offset + diff between tilesize and snake segment size
->>>>>>> 3a552c1886d76bd23382b54e3b2cd5b70e5954e8
+
 }
 
 function startGame(){
     document.getElementById("start").style.display = "none";
     document.getElementById("game-over").style.display = "none";
+    document.getElementById("score").innerHTML = "Score: 0";
 
     intervalId = setInterval(move, Math.round((1/fps)*1000));
 
